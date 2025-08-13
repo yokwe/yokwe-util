654c5734c26b284c3aac3d701e11a6b5daddec13
hasegawa <hasegawa@localhost>
2025-08-13 11:05:21 +0900

    Use Storage in StorageSelenium

--- src/main/java/yokwe/util/selenium/StorageSelenium.java
+++ src/main/java/yokwe/util/selenium/StorageSelenium.java
@@ -1,101 +1,20 @@
 package yokwe.util.selenium;
 
-import java.io.File;
-
-import yokwe.util.FileUtil;
-import yokwe.util.UnexpectedException;
+import yokwe.util.Storage;
 
 public interface StorageSelenium {
-	// interface requires logger must be public static final
-	public static final org.slf4j.Logger logger = yokwe.util.LoggerUtil.getLogger();
+	public static final Storage storage = Storage.storage.getStorage("selenium");
 	
-	public String getPath();
-	public String getPath(String path);
-	public String getPath(String prefix, String path);
-	
-	default public File getFile() {
-		return new File(getPath());
-	}
-	default public File getFile(String path) {
-		return new File(getPath(path));
-	}
-	default public File getFile(String prefix, String path) {
-		return new File(getPath(prefix, path));
-	}
-	
-	
-	
-	public static final String SELENIUM_DATA_PATH_FILE = "data/SeleniumDataPathLocation";
-	private static String getSeleniumDataPath() {		
-		logger.info("SELELIUM_DATA_PATH_FILE  !{}!", SELENIUM_DATA_PATH_FILE);
-		// Sanity check
-		if (!FileUtil.canRead(SELENIUM_DATA_PATH_FILE)) {
-			logger.error("Cannot read file");
-			throw new UnexpectedException("Cannot read file");
-		}
-		
-		String dataPath = FileUtil.read().file(SELENIUM_DATA_PATH_FILE);
-		logger.info("SELENIUM_DATA_PATH       !{}!", dataPath);
-		// Sanity check
-		if (dataPath.isEmpty()) {
-			logger.error("Empty dataPath");
-			throw new UnexpectedException("Empty dataPath");
-		}		
-		if (!FileUtil.isDirectory(dataPath)) {
-			logger.error("Not directory");
-			throw new UnexpectedException("Not directory");
-		}		
-		return dataPath;
-	}
-	public static final String SELENIUM_DATA_PATH = getSeleniumDataPath();
-	public class Impl implements StorageSelenium {
-		org.slf4j.Logger logger = yokwe.util.LoggerUtil.getLogger();
-
-		private final String basePath;
-		
-		public Impl(String basePath) {
-			if (!FileUtil.isDirectory(basePath)) {
-				logger.error("Not directory");
-				logger.error("  basePath  {}!", basePath);
-				throw new UnexpectedException("Not directory");
-			}
-
-			this.basePath = basePath;
-		}
-		public Impl(String parent, String prefix) {
-			this(parent + "/" + prefix);
-		}
-		public Impl(StorageSelenium parent, String prefix) {
-			this(parent.getPath(), prefix);
-		}
-		
-		@Override
-		public String getPath() {
-			return basePath;
-		}
-
-		@Override
-		public String getPath(String path) {
-			return basePath + "/" + path;
-		}
-
-		@Override
-		public String getPath(String prefix, String path) {
-			return basePath + "/" + prefix + "/" + path;
-		}
-	}
-	
-	
-	public static final StorageSelenium root             = new Impl(SELENIUM_DATA_PATH);
-	public static final StorageSelenium chromeForTesting = new Impl(root, "chrome-for-testing");
+	public static final Storage chromeForTesting = storage.getStorage("chrome-for-testing");
 	
 	public static void main(String[] args) {
+		var logger = yokwe.util.LoggerUtil.getLogger();
+		
 		logger.info("START");
 		
-		logger.info("root             {}", StorageSelenium.root.getPath());
-		logger.info("chromeForTesting {}", StorageSelenium.chromeForTesting.getPath());
+		logger.info("storage          {}", StorageSelenium.storage.getFile());
+		logger.info("chromeForTesting {}", StorageSelenium.chromeForTesting.getFile());
 		
 		logger.info("STOP");
 	}
-
 }
